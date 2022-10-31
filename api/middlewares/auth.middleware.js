9f786e5b7198a0556074c28f9e720c69bc13929d
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-10-31 09:59:41 -0300

    middlewares

---
+++ api/middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import { findByIdUserService } from "../services/user.service.js";
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "The token was not informed!" });
+  }
+
+  const parts = authHeader.split(" "); 
+
+  if (parts.length !== 2) {
+    return res.status(401).send({ message: "Invalid token!" });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ message: "Malformatted Token!" });
+  }
+
+  jwt.verify(token, process.env.SECRET_JWT , async (err, decoded) => {
+    
+    if(err) {
+      console.error(err);
+      return res.status(401).send({ message: "Invalid token!" });
+    }
+
+    const user = await findByIdUserService(decoded.id);
+
+    if (!user || !user.id) {
+      return res.status(401).send({ message: "Invalid token!" });
+    }
+
+    req.userId = user.id;
+
+    return next();
+  });
+};
+
+export default authMiddleware;
