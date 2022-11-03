0007940609d866d49bd468624ed77d2d914505c2
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-03 09:04:16 -0300

    updating index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-import connectDatabase from  "./api/data/conect.js";
-import userRoute from  "./api/routes/user.route.js";
-import authRoute from  "./api/routes/auth.route.js";
-import postRoute from  "./api/routes/post.route.js";
-import express from  "express";
-import cors from  "cors";
-import dotenv from "dotenv";
-
+import express from "express";
+import connectDatabase from "./api/database/db.js"
+import dotenv from "dotenv"
+import homeRouter from "./api/routes/home.route.js"
+import userRouter from "./api/routes/user.route.js"
+import authRouter from "./api/routes/auth.route.js"
+import postRouter from "./api/routes/post.route.js"
+import uploadRouter from "./api/routes/upload.route.js"
+import cors from 'cors';
 dotenv.config();
-
-const website = process.env.WEBSITE_URL;
-const port = process.env.PORT || 3700;
 const app = express();
+const port = process.env.PORT || 3700;
 
 connectDatabase();
-app.use(cors(website));
+app.use(cors());
 app.use(express.json());
-app.use("/user", userRoute);
-app.use("/auth", authRoute);
-app.use("/posts", postRoute);
+
+app.use("/",homeRouter);
+app.use("/users",userRouter);
+app.use("/auth",authRouter);
+app.use("/posts",postRouter);
+app.use("/upload",uploadRouter);
 
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+app.listen(port,()=>console.log(`[Server Online]: PORT:${port}`));
